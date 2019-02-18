a8db7a3ce4f60515fae9be9e2946233bc6be2fb2
Simon <01secn@gmail.com>
2019-02-18 15:40:00 +0000

    Added submit button to specify cat name

--- app.rb
+++ app.rb
@@ -8,7 +8,12 @@ get '/secret' do
   'The cake is not a lie'
 end
 
-get '/cat' do
+get '/random-cat' do
   @randomname = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  @randomname = params[:name]
   erb :index
 end
