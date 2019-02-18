7df55cf9e841b304d4773e5e33005d1a1f5eaac6
Oliver <ofcharrison@gmail.com>
2019-02-18 15:13:11 +0000

    Your Turn/ up to sinatra params

--- app.rb
+++ app.rb
@@ -9,5 +9,6 @@ get '/secret' do
 end
 
 get '/cat' do
-  '<img style="border: dashed red" src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png"></img>'
+  @randomname = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
 end
